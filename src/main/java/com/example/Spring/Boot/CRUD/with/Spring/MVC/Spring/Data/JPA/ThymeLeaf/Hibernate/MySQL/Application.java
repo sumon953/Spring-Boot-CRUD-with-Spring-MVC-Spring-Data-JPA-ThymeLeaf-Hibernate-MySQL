f6570d265fab46a5f21e315be89abc3220d2919c
43031 <sumonsorder953@gmail.com>
2021-03-03 17:46:12 +0600

    First

---
+++ src/main/java/com/example/Spring/Boot/CRUD/with/Spring/MVC/Spring/Data/JPA/ThymeLeaf/Hibernate/MySQL/Application.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Boot.CRUD.with.Spring.MVC.Spring.Data.JPA.ThymeLeaf.Hibernate.MySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring/Boot/CRUD/with/Spring/MVC/Spring/Data/JPA/ThymeLeaf/Hibernate/MySQL/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Boot.CRUD.with.Spring.MVC.Spring.Data.JPA.ThymeLeaf.Hibernate.MySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
