0600eef04b09eb41bf51c79dc0b2b5c31e82bc32
43031 <sumonsorder953@gmail.com>
2021-03-04 12:16:42 +0600

    Complete

--- src/main/java/com/example/Spring/Boot/CRUD/with/Spring/MVC/Spring/Data/JPA/ThymeLeaf/Hibernate/MySQL/controller/AppController.java
+++ src/main/java/com/example/Spring/Boot/CRUD/with/Spring/MVC/Spring/Data/JPA/ThymeLeaf/Hibernate/MySQL/controller/AppController.java
@@ -5,8 +5,11 @@ import com.example.Spring.Boot.CRUD.with.Spring.MVC.Spring.Data.JPA.ThymeLeaf.Hi
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -16,6 +19,7 @@ public class AppController {
     @Autowired
     private ProductService productService;
 
+
     @RequestMapping("/")
     public String ViewHomePage(Model model)
     {
@@ -23,5 +27,45 @@ public class AppController {
         model.addAttribute("listProduct",listProduct);
         return "index";
     }
+
+
+    @RequestMapping("/new")
+    public String showNewProductForm(Model model)
+    {
+        Product product = new Product();
+        model.addAttribute("product",product);
+        return "new_product";
+
+    }
+
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveProduct(@ModelAttribute("product") Product product)
+    {
+       productService.save(product);
+       return "redirect:/";
+    }
+
+
+
+    @RequestMapping("/edit/{id}")
+    public ModelAndView showEditProductForm(@PathVariable(name = "id") Long id)
+    {
+        ModelAndView mav = new ModelAndView("edit_product");
+        Product product = productService.get(id);
+        mav.addObject("product",product);
+        return mav;
+
+    }
+
+
+    @RequestMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable(name = "id") Long id)
+    {
+        productService.delete(id);
+        return "redirect:/";
+
+    }
+
 
 }
